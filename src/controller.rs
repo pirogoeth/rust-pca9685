c0624b09ca8209641b8215adcc8d64a3b70fe63c
Sean Johnson <sean@mailgun.com>
2018-06-22 23:41:31 -0500

    Documentation, controller initial set-up, and PWM rate setter

--- src/controller.rs
+++ src/controller.rs
@@ -1,7 +1,29 @@
+use env_logger;
 use i2cdev::core::*;
+use std::{thread, time};
 
 use ::constants;
 use ::led_channel::LEDChannel;
+
+/// Calculates a value to insert into PRE_SCALE register where
+/// `update_rate` is the output modulation frequency in Hertz.
+/// 
+/// Implemented from the [PCA9685 datasheet](https://cdn-shop.adafruit.com/datasheets/PCA9685.pdf#G4466866).
+/// 
+/// # Examples
+/// 
+/// ```
+/// # extern crate rust_pca9685;
+/// # use rust_pca9685::controller::calculate_prescale_value;
+/// #
+/// let update_rate = 200.0; // update rate in hz
+/// let prescale_value = calculate_prescale_value(update_rate);
+/// assert_eq!(0x1e, prescale_value);
+/// ```
+pub fn calculate_prescale_value(update_rate: f32) -> u8 {
+    let base_val = constants::OSCILLATION_FREQ / (constants::STEP_SIZE * update_rate);
+    return (base_val.round() - 1.0) as u8;
+}
 
 #[derive(Debug)]
 pub struct Controller<'a, T: I2CDevice + 'a> {
@@ -11,9 +33,82 @@ pub struct Controller<'a, T: I2CDevice + 'a> {
 impl<'a, T: I2CDevice + 'a> Controller<'a, T> {
 
     pub fn new(dev: &'a mut T) -> Controller<'a, T> {
-        Controller{
-            device: dev,
+        let mut c = Controller{ device: dev };
+        {
+            c.set_up().unwrap();
         }
+
+        return c;
+    }
+
+    /// Performs some initial set up on the PCA9685.
+    /// - Set `OUTDRV` on `MODE_2`
+    /// - Set `ALLCALL` on `MODE_1`
+    /// - Sleep waiting on oscillator
+    /// - Read `MODE_1` back
+    /// - Unset the `SLEEP` bit on `MODE_1` to wake up controller
+    /// - Sleep waiting on oscillator
+    /// 
+    /// This puts the controller `MODE_*` registers into a known state at
+    /// the beginning of operation.
+    /// 
+    /// At the beginning of operation, you can expect:
+    /// - `MODE_1` *should* be set to `0x01` (ALL_CALL)
+    /// - `MODE_2` *should* be set to `0x04` (OUTDRV)
+    fn set_up(&mut self) -> Result<(), T::Error> {
+        self.device.smbus_write_byte_data(constants::MODE_2, constants::OUTDRV)?;
+        self.device.smbus_write_byte_data(constants::MODE_1, constants::ALL_CALL)?;
+        thread::sleep(time::Duration::from_millis(5));
+
+        let mode1 = self.device.smbus_read_byte_data(constants::MODE_1)?;
+        let mode1 = mode1 & (!constants::SLEEP);
+        self.device.smbus_write_byte_data(constants::MODE_1, mode1)?;
+        thread::sleep(time::Duration::from_millis(5));;
+
+        Ok(())
+    }
+
+    /// Sets the controller's output modulation rate.
+    /// Expects `prescale_value` to be a value that has been calculated
+    /// by [rust_pca9685::controller::calculate_prescale_value][calculate_prescale_value].
+    /// 
+    /// After `set_pwm_rate`, `MODE_1` will also have the `RESTART` bit set so channel
+    /// values will persist even if the controller is put to `SLEEP`.
+    /// 
+    /// [calculate_prescale_value]: fn.calculate_prescale_value.html
+    pub fn set_pwm_rate(&mut self, prescale_value: u8) -> Result<(), T::Error> {
+        let _ = env_logger::try_init();
+
+        // Save the old controller mode for revert
+        let old_mode = self.device.smbus_read_byte_data(constants::MODE_1)?;
+        debug!("MODE_1 is {:#04x} before going to sleep", old_mode);
+
+        // Make a new mode value to sleep the controller
+        // NOTE: `0x7f` carries the `EXTCLK`, `AI`, `SLEEP`, `SUB1`, `SUB2`, `SUB3`, `ALL_CALL` bits
+        // over to the new mode, intentionally eliding over the `RESTART` value
+        let new_mode = (old_mode & 0x7f) | constants::SLEEP;
+
+        // Effectively put the controller to sleep
+        self.device.smbus_write_byte_data(constants::MODE_1, new_mode)?;
+        debug!("wrote {:#04x} to MODE_1", new_mode);
+
+        // Write `prescale_value` to the `PRE_SCALE` register
+        debug!("setting output modulation frequency to {} (prescale)", prescale_value);
+        self.device.smbus_write_byte_data(constants::PRE_SCALE, prescale_value)?;
+
+        // Restore the old `MODE_1` flags
+        self.device.smbus_write_byte_data(constants::MODE_1, old_mode)?;
+        debug!("restored {:#04x} to MODE_1", old_mode);
+
+        // Wait for oscillator to stabilize before setting `RESTART`
+        thread::sleep(time::Duration::from_millis(5));
+
+        // Set restart bit; forces all channels to remain in their state when the clock is off
+        let restart_mode = old_mode | constants::RESTART;
+        self.device.smbus_write_byte_data(constants::MODE_1, restart_mode)?;
+        debug!("enable restart mode sets MODE_1 {:#04x}", restart_mode);
+
+        Ok(())
     }
 
     /// Set `channel_num`'s registers to the on/off values given.
--- tests/controller.rs
+++ tests/controller.rs
@@ -4,17 +4,65 @@ extern crate log;
 extern crate env_logger;
 extern crate rust_pca9685;
 
+use i2cdev::core::I2CDevice;
 use i2cdev::mock::MockI2CDevice;
 
 use rust_pca9685::{
-    controller::Controller,
+    constants,
+    controller::{
+        calculate_prescale_value,
+        Controller,
+    },
     led_channel::LEDChannel,
 };
 
+#[test]
+fn test_calculate_prescale_values() {
+    assert_eq!(0x1e, calculate_prescale_value(200f32));
+}
 
 #[test]
-// This test references values used in the [PCA9685 datasheet](https://cdn-shop.adafruit.com/datasheets/PCA9685.pdf).MockI2CDevice
-// Specifically, values from Example 1 in Section 7.3.3 are used as a base case.
+fn test_controller_init() {
+    let _ = env_logger::try_init();
+
+    let mut device = MockI2CDevice::new();
+
+    // Write the default values to `MODE_*` registers
+    device.smbus_write_byte_data(constants::MODE_1, 0x11).unwrap();
+    device.smbus_write_byte_data(constants::MODE_2, 0x04).unwrap();
+
+    // Run the controller initialization step
+    Controller::new(&mut device);
+
+    let mode1 = device.smbus_read_byte_data(constants::MODE_1).unwrap();
+    let mode2 = device.smbus_read_byte_data(constants::MODE_2).unwrap();
+
+    assert_eq!(0x01, mode1);
+    assert_eq!(0x04, mode2);
+}
+
+#[test]
+fn test_controller_set_pwm_rate() {
+    let _ = env_logger::try_init();
+
+    let prescale_value = calculate_prescale_value(60f32);
+
+    let mut device = MockI2CDevice::new();
+
+    {
+        let mut ctrl = Controller::new(&mut device);
+        ctrl.set_pwm_rate(prescale_value).unwrap();
+    }
+
+    let mode1 = device.smbus_read_byte_data(constants::MODE_1).unwrap();
+    let prescale_reg = device.smbus_read_byte_data(constants::PRE_SCALE).unwrap();
+    assert_eq!(constants::ALL_CALL | constants::RESTART, mode1);
+    assert_eq!(prescale_value, prescale_reg);
+}
+
+#[test]
+/// This test references values used in the [PCA9685 datasheet](https://cdn-shop.adafruit.com/datasheets/PCA9685.pdf).MockI2CDevice
+/// Specifically, values from Example 1 in Section 7.3.3 are used as a base case.
 fn test_write_value_to_channel() {
     let _ = env_logger::try_init();
 
